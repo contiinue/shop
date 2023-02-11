478bf39fd38dfbcbf7f958a720bb430bf5fe5807
main <shapilov04@mail.ru>
2023-02-11 19:34:28 +0300

    build: Added Order View for bay > 2 items

--- src/apps/product/urls.py
+++ src/apps/product/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path, include
-from .views import ItemView, SuccessPayment, CancelPayment
+from .views import ItemView, SuccessPayment, CancelPayment, OrderView
 
 urlpatterns = [
     path("item/<int:pk>", ItemView.as_view()),
-    path('cancel/', CancelPayment.as_view(), name='cansel-payment'),
-    path('success/', SuccessPayment.as_view(), name='success-payment')
+    path("order/<int:pk>", OrderView.as_view()),
+    path("cancel/", CancelPayment.as_view(), name="cansel-payment"),
+    path("success/", SuccessPayment.as_view(), name="success-payment"),
 ]
--- src/apps/product/views.py
+++ src/apps/product/views.py
@@ -1,3 +1,4 @@
+from django.db.models import Count, Sum, F
 from django.views.generic import DetailView, TemplateView
 
 from config.settings import STRIPE_PUBLIC_KEY
@@ -18,12 +19,33 @@ class ItemView(DetailView):
 
 class OrderView(DetailView):
     model = Order
-    template_name = ''
+    template_name = "product/order.html"
+
+    def get_queryset(self):
+        return (
+            Order.objects.select_related("discount", "tax")
+            .filter(pk=self.kwargs["pk"])
+            .annotate(
+                quantity=Count("items"),
+                amount=Sum(
+                    F("items__price")
+                    - (F("items__price") / 100 * F("discount__discount_percentage"))
+                    - (F("items__price") / 100 * F("tax__tax_percentage"))
+                ),
+            )
+        )
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["currency"] = "usd"
+        context["currency_form"] = CurrencyForm()
+        context["STRIPE_PUBLIC_KEY"] = STRIPE_PUBLIC_KEY
+        return context
 
 
 class SuccessPayment(TemplateView):
-    template_name = 'product/success-payment.html'
+    template_name = "product/success-payment.html"
 
 
 class CancelPayment(TemplateView):
-    template_name = 'product/cancel-payment.html'
+    template_name = "product/cancel-payment.html"
